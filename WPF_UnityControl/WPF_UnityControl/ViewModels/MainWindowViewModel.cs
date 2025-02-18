a37ad14259edb086607f4261ed433ddfa0b9f685
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-02-18 23:03:36 +0900

    MaterialDesign初期定義

---
+++ WPF_UnityControl/WPF_UnityControl/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_UnityControl.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public ReactiveProperty<string> Title { get; } = new("UnityControl");
+    }
+}
