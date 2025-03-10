452ec9bfc656db0560e431a46793d1e32c8524a7
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-11 01:41:06 +0900

    ファイル操作をするUserControlの追加(仮)

--- WPF_UnityControl/WPF_UnityControl/Views/UserControls/FileHandlerControl.xaml.cs
+++ WPF_UnityControl/WPF_UnityControl/Views/UserControls/FileHandlerControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPF_UnityControl.Controls
+namespace WPF_UnityControl.Control
 {
     /// <summary>
     /// FileHandlerControl.xaml の相互作用ロジック
