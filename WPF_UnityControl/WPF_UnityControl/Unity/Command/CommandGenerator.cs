817839e8b038fd5a8095990aad80bf2dd6191bcd
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-13 02:55:30 +0900

    コマンドに応じてコマンド専用のJsonファイルを作成するクラスの作成

---
+++ WPF_UnityControl/WPF_UnityControl/Unity/Command/CommandGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WPF_UnityControl.Unity
+{
+    public class CommandGenerator
+    {
+        /// <summary>
+        /// コマンドタイプからUnity送信用コマンド(Json)を作成
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public string GenerateJsonCommand(CommandType cmd, string[]? parameters =null)
+        {
+            //Json作成
+            var commandData = new
+            {
+                Command = cmd.ToString(),
+                Parameters = parameters ?? new string[0]
+            };
+
+            return JsonConvert.SerializeObject(commandData);
+        }
+    }
+}
