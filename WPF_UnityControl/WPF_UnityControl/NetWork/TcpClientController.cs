ffd54c74ba5a558366ed8b6fe0e8e29ed07ae5fe
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-13 02:52:21 +0900

    Tcpの接続状態を管理するコントローラークラスの作成

---
+++ WPF_UnityControl/WPF_UnityControl/NetWork/TcpClientController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_UnityControl.NetWork
+{
+    public class TcpClientController
+    {
+        private readonly UnityTcpClient _tcp;
+
+        public TcpClientController(UnityTcpClient tcp)
+        {
+            _tcp = tcp;
+        }
+
+        public async Task BeginSocketWrite(string cmd)
+        {
+            if (_tcp == null || cmd.Length < 1) 
+                return;
+
+            try
+            {
+                await _tcp.SendCommandAsync(cmd);
+            }
+            catch(SocketException ex)
+            {
+                MessageBox.Show($"サーバーへの送信中にエラーが発生しました : {ex.Message} \n 接続を確認してください。");
+            }
+        }
+    }
+}
