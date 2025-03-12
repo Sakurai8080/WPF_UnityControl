219bb660bf50501f9762b1bb7eeedf40a04b237b
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-13 02:54:40 +0900

    Unityとのコマンド送受信(通信)するクラスの作成

---
+++ WPF_UnityControl/WPF_UnityControl/Unity/Command/UnityCommandDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_UnityControl.NetWork;
+
+namespace WPF_UnityControl.Unity
+{
+    public class UnityCommandDispatcher
+    {
+
+        private readonly TcpClientController _tcpController;
+        private readonly CommandGenerator _cmdGenerator;
+
+        public UnityCommandDispatcher(TcpClientController tcpController)
+        {
+            _tcpController = tcpController;
+            _cmdGenerator = new CommandGenerator();
+        }
+
+        public void SendCommand(CommandType cmd, string[]? parameters = null)
+        {
+            var cmdJson = _cmdGenerator.GenerateJsonCommand(cmd, parameters);
+            _ = _tcpController.BeginSocketWrite(cmdJson);
+        }
+    }
+}
