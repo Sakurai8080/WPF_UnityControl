c076336341008b0755fc046ecd3eb12eef6f4790
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-23 21:07:06 +0900

    Unity送信用のCommandPOCOクラスの作成

---
+++ WPF_UnityControl/WPF_UnityControl/JsonPoco/JsonCommand.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_UnityControl
+{
+    /// <summary>
+    /// コマンド作成用JsonPOCOクラス
+    /// </summary>
+    public class JsonCommand
+    {
+        [JsonProperty("Command")]
+        public string Command { get; set; }
+
+        [JsonProperty("Parameters")]
+        public string[] Parameters { get; set; }
+
+        public JsonCommand(string command, string[] parameters)
+        {
+            Command = command;
+            Parameters = parameters;
+        }
+    }
+}
