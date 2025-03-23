1143c93828d24b41a28122a7f3556a2f98b72d46
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-24 02:52:45 +0900

    Prismの土台構築。MainWindowにPrismを使用してUserControlの組み込み

--- WPF_UnityControl/WPF_UnityControl/App.xaml.cs
+++ WPF_UnityControl/WPF_UnityControl/App.xaml.cs
@@ -4,6 +4,8 @@ using Prism.Unity;
 using Prism.Ioc;
 using System.Windows;
 using WPF_UnityControl.Views;
+using WPF_UnityControl.ViewModels;
+using WPF_UnityControl.Control;
 
 namespace WPF_UnityControl;
 
@@ -12,16 +14,18 @@ namespace WPF_UnityControl;
 /// </summary>
 public partial class App : PrismApplication
 {
-
-    protected override void RegisterTypes(IContainerRegistry containerRegistry)
-    {
-        // DIコンテナに型追加
-    }
-
     protected override Window CreateShell()
     {
-        var window = Container.Resolve<MainWindow>();
-        return window;
+        return Container.Resolve<MainWindow>();
+    }
+
+    /// <summary>
+    /// コンテナへの型登録。ViewとViewModelの紐づけ
+    /// </summary>
+    /// <param name="containerRegistry"></param>
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.RegisterForNavigation<MainContentPage, MainContentPageViewModel>();
     }
 }
 
