daa97d76266d806317bcc3000064da21a7fe4630
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-25 23:54:25 +0900

    ファサードの役割をする核となるコントールクラスの追加

---
+++ WPF_UnityControl/WPF_UnityControl/Facades/UnityController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_UnityControl.Unity;
+
+namespace WPF_UnityControl.Facades
+{
+    public class UnityController
+    {
+        private UnityCommandDispatcher _unityDsp;
+
+        public UnityController()
+        {
+            _unityDsp = new UnityCommandDispatcher();
+        }
+
+        public void UnityConnetChange()
+        {
+            _ = _unityDsp.TCPController.ConnectToUnityAsync();
+        }
+
+        private void SetObjectPosition()
+        {
+            string[] prm = { "x : 1", "y : 2", " z : 3" };
+            //_unityDsp.BeginSendCommand(CommandType.OBJECT_MOVE, prm);
+        }
+    }
+}
