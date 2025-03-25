f0a56a4d01e61b82694334c578edccecd9ed6d80
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-25 23:55:39 +0900

    クラス名の誤字修正

---
+++ WPF_UnityControl/WPF_UnityControl/Views/UserControls/UnityOperationControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WPF_UnityControl.Control
+{
+    /// <summary>
+    /// UnityConnectTestControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UnityOperationControl : UserControl
+    {
+        public UnityOperationControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
