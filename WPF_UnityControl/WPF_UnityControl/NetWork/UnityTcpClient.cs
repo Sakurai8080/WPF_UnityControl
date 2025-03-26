cd6b6e9d0b32184ddd89e4d72210f61475e6e1e9
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-03-27 01:47:58 +0900

    Unityから返答があった場合のイベント処理を追加

--- WPF_UnityControl/WPF_UnityControl/NetWork/UnityTcpClient.cs
+++ WPF_UnityControl/WPF_UnityControl/NetWork/UnityTcpClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -34,6 +35,10 @@ namespace WPF_UnityControl.NetWork
         public bool IsConnected => _client?.Connected ?? false;
         #endregion
 
+        #region デリゲート
+        /// <summary> 受信完了イベント </summary>
+        public Action<string> OnReceived = (json) => { };
+        #endregion
         /// <summary>
         /// 接続処理
         /// </summary>
@@ -48,6 +53,9 @@ namespace WPF_UnityControl.NetWork
                 _client = new TcpClient();
                 await _client.ConnectAsync(SERVER_IP, SERVER_PORT);
                 _stream = _client.GetStream();
+                _ = ReceiveLoopAsync(); // 受信監視開始
+
+                Debug.WriteLine($"接続完了 :{SERVER_IP} - {SERVER_PORT}");
                 MessageBox.Show(App.Current.MainWindow, $"接続完了 :{SERVER_IP} - {SERVER_PORT}");
             }
             catch (SocketException e)
@@ -55,11 +63,45 @@ namespace WPF_UnityControl.NetWork
                 MessageBox.Show(App.Current.MainWindow, $"サーバー接続時にエラーが発生しました : {e.Message}");
             }
         }
+
+        /// <summary>
+        /// Unityサーバーからのデータを非同期で受信
+        /// </summary>
+        /// <returns></returns>
+        private async Task ReceiveLoopAsync()
+        {
+            if (_stream == null)
+                return;
+
+            var buffer = new byte[4096];
+            var stringBuilder = new StringBuilder();
+
+            try
+            {
+                while (_client?.Connected == true)
+                {
+                    int bytesRead = await _stream.ReadAsync(buffer, 0, buffer.Length);
+                    if (bytesRead == 0) break;
+
+                    string receivedText = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    if (!string.IsNullOrEmpty(receivedText))
+                    {
+                        OnReceived?.Invoke(receivedText);
+                    }
+                }
+            }
+            catch( Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
      
         public void Close()
         {
             _stream?.Close();
             _client?.Close();
+            _stream = null;
+            _client = null;
         }
         public void Dispose()
         {
