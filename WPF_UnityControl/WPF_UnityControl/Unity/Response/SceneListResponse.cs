0fbdbc2a2bdddf8a79b89fecf0735a06c3bed5fb
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-04-04 02:48:48 +0900

    シーン一覧用レスポンスクラスの追加

---
+++ WPF_UnityControl/WPF_UnityControl/Unity/Response/SceneListResponse.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using WPF_UnityControl.Interface;
+
+namespace WPF_UnityControl.Unity
+{
+    public class SceneListResponse : IResponseData
+    {
+
+        /// <summary> Unityシーン一覧 </summary>
+        public ReactivePropertySlim<List<string>> SceneList { get; } = new ReactivePropertySlim< List<string>>(new List<string>());
+
+        /// <summary>
+        /// Jsonファイルを基に処理を実行
+        /// </summary>
+        /// <param name="json">Unityから受け取ったJsonファイル</param>
+        public void Execute(string json)
+        {
+            var scenes = JsonConvert.DeserializeObject<string[]>(json);
+            if (scenes?.Length >= 1)
+            {
+                ResponseToList(scenes);
+            }
+        }
+
+        /// <summary>
+        /// Jsonファイルのシーン一覧をコレクションに格納
+        /// </summary>
+        /// <param name="scenes"></param>
+        public void ResponseToList(string[] scenes)
+        {
+            var sceneList = scenes?.ToList() ?? new();
+            SceneList.Value = sceneList;
+        }
+    }
+}
