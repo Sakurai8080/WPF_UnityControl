b42d2c7e0c15f7d4ea157e49de741fb4de980ab7
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-04-19 17:20:34 +0900

    レスポンスクラス用のベースクラスの追加

---
+++ WPF_UnityControl/WPF_UnityControl/Base/BaseResponse.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WPF_UnityControl.Base
+{
+    /// <summary>
+    /// レスポンスを処理する基底クラス
+    /// </summary>
+    public abstract class BaseResponse
+    {
+        protected IEventAggregator _eventAggregator;
+        protected BaseResponse(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+        }
+    }
+}
