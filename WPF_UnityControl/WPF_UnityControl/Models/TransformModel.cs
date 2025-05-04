78ef1fe0dabb4100babc513031f97999f4f6ff2e
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-05-04 17:19:59 +0900

    オブジェクトのトランスフォームデータクラスの追加

---
+++ WPF_UnityControl/WPF_UnityControl/Models/TransformModel.cs
@@ -0,0 +1,23 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_UnityControl.Models
+{
+    public　class TransformModel
+    {
+        public string ObjectPosX { get; set; }
+        public string ObjectPosY { get; set; }
+        public string ObjectPosZ { get; set; }
+        public string ObjectRotX { get; set; }
+        public string ObjectRotY { get; set; }
+        public string ObjectRotZ { get; set; }
+        public string ObjectScaX { get; set; }
+        public string ObjectScaY { get; set; }
+        public string ObjectScaZ { get; set; }
+
+    }
+}
