21506180b25b76c669907ea48ae26b20fef1f77d
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-05-30 02:28:29 +0900

    ゲームオブジェクトデータ格納用モデルクラスの追加

---
+++ WPF_UnityControl/WPF_UnityControl/Models/GameObjectModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_UnityControl.Models
+{
+    public class GameObjectModel
+    {
+        public string Name { get; set; }
+
+        public string Tag { get; set; }
+
+        public string Layer { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public TransformModel Transform { get; }
+    }
+}
---
+++ WPF_UnityControl/WPF_UnityControl/Models/Vector3Model.cs
@@ -0,0 +1,16 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_UnityControl.Models
+{
+    public class Vector3Model
+    {
+        public ReactivePropertySlim<float> X { get; } = new ReactivePropertySlim<float>();
+        public ReactivePropertySlim<float> Y { get; } = new ReactivePropertySlim<float>();
+        public ReactivePropertySlim<float> Z { get; } = new ReactivePropertySlim<float>();
+    }
+}
