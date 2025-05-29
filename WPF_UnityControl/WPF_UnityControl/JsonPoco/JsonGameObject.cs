adcc8177768c02f4c1b7cab7f8ace52c2ca1e6e8
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-05-30 02:27:22 +0900

    GameObjectデータ格納用JsonPOCO追加

---
+++ WPF_UnityControl/WPF_UnityControl/JsonPoco/JsonGameObject.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace WPF_UnityControl.JsonPoco
+{
+    public class JsonGameObject
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Tag")]
+        public string Tag { get; set; }
+
+        [JsonProperty("Layer")]
+        public string Layer { get; set; }
+
+        [JsonProperty("IsActive")]
+        public bool IsActive { get; set; }
+
+        [JsonProperty("Transform")]
+        public JsonTransform Transform { get; set; }
+    }
+
+    public class JsonTransform
+    {
+        [JsonProperty("Position")]
+        public JsonVector3 Position { get; set; }
+
+        [JsonProperty("Rotation")]
+        public JsonVector3 Rotation { get; set; }
+
+        [JsonProperty("Scale")]
+        public JsonVector3 Scale { get; set; }
+    }
+
+    public class JsonVector3
+    {
+        [JsonProperty("X")]
+        public float X { get; set; }
+
+        [JsonProperty("Y")]
+        public float Y { get; set; }
+        [JsonProperty("Z")]
+        public float Z { get; set; }
+    }
+}
