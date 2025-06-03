78e6b3903555f7395d76079f5a908870abc74560
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-06-04 03:42:04 +0900

    ゲームオブジェクトデータ変更イベントの追加

--- WPF_UnityControl/WPF_UnityControl/Events/ObjectEvent.cs
+++ WPF_UnityControl/WPF_UnityControl/Events/ObjectEvent.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WPF_UnityControl.Models;
 
 namespace WPF_UnityControl.Events
 {
-    public class ObjTransformChangeObject : PubSubEvent<TransformModel> { };
+    public class GameObjectDataFetchedEvent : PubSubEvent<GameObjectModel> { }
 }
