8d2379e596d09cb38059bfce40e3fd86a4bf78ac
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-06-18 02:22:33 +0900

    ベースクラスのリファクタリング

--- WPF_UnityControl/WPF_UnityControl/Base/BaseResponse.cs
+++ WPF_UnityControl/WPF_UnityControl/Base/BaseResponse.cs
@@ -1,15 +1,21 @@
-﻿
-namespace WPF_UnityControl.Base
+﻿namespace WPF_UnityControl.Base
 {
     /// <summary>
     /// レスポンスを処理する基底クラス
     /// </summary>
     public abstract class BaseResponse
     {
+        #region フィールド
+        /// <summary>
+        /// イベント通信の管理
+        /// </summary>
         protected IEventAggregator _eventAggregator;
+        #endregion
+        #region コンストラクタ
         protected BaseResponse(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
         }
+        #endregion
     }
 }
