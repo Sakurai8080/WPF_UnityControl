1c55030c7e58301eddb8161013fe3b6b74b7053c
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-06-18 02:40:31 +0900

    イベントクラスのコメント追加

--- WPF_UnityControl/WPF_UnityControl/Events/SceneEvent.cs
+++ WPF_UnityControl/WPF_UnityControl/Events/SceneEvent.cs
@@ -2,8 +2,18 @@
 
 namespace WPF_UnityControl.Events
 {
-    /// <summary> シーン一覧取得時の発行イベント </summary>
+    /// <summary>
+    /// シーン一覧更新通知イベント
+    /// </summary>
     public class SceneListUpdateEvent : PubSubEvent<List<string>>{}
+
+    /// <summary>
+    /// 選択中のシーン取得通知イベント
+    /// </summary>
     public class SceneNameChangedEvent : PubSubEvent<string> { }
+
+    /// <summary>
+    /// ゲームオブジェクトのヒエラルキー取得通知イベント
+    /// </summary>
     public class HierarchyFetchedEvent : PubSubEvent<List<HierarchyNode>> { }
 }
