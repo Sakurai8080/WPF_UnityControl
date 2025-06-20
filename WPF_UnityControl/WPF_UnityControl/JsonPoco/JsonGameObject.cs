30dd8ba472933f2b3be59d4481c939af62caeb79
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-06-21 00:27:08 +0900

    POCOクラスのリファクタリング

--- WPF_UnityControl/WPF_UnityControl/JsonPoco/JsonGameObject.cs
+++ WPF_UnityControl/WPF_UnityControl/JsonPoco/JsonGameObject.cs
@@ -2,43 +2,86 @@
 
 namespace WPF_UnityControl.JsonPoco
 {
+    /// <summary>
+    /// ゲームオブジェクトデータ
+    /// </summary>
     public class JsonGameObject
     {
+        /// <summary>
+        /// ゲームオブジェクト名
+        /// </summary>
         [JsonProperty("name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// オブジェクトタグ
+        /// </summary>
         [JsonProperty("tag")]
         public string Tag { get; set; }
 
+        /// <summary>
+        /// オブジェクトレイヤー
+        /// </summary>
         [JsonProperty("layer")]
         public string Layer { get; set; }
 
+        /// <summary>
+        /// アクティブフラグ
+        /// </summary>
         [JsonProperty("isActive")]
         public bool IsActive { get; set; }
 
+        /// <summary>
+        /// トランスフォームデータ
+        /// </summary>
         [JsonProperty("transform")]
         public JsonTransform Transform { get; set; }
     }
 
+    /// <summary>
+    /// トランスフォームデータ
+    /// </summary>
     public class JsonTransform
     {
+        /// <summary>
+        /// ポジション
+        /// </summary>
         [JsonProperty("position")]
         public JsonVector3 Position { get; set; }
 
+        /// <summary>
+        /// 回転
+        /// </summary>
         [JsonProperty("rotation")]
         public JsonVector3 Rotation { get; set; }
 
+        /// <summary>
+        /// サイズ
+        /// </summary>
         [JsonProperty("scale")]
         public JsonVector3 Scale { get; set; }
     }
 
+    /// <summary>
+    /// Vector3データ
+    /// </summary>
     public class JsonVector3
     {
+        /// <summary>
+        /// Vector3 X
+        /// </summary>
         [JsonProperty("x")]
         public float X { get; set; }
 
+        /// <summary>
+        /// Vector3 Y
+        /// </summary>
         [JsonProperty("y")]
         public float Y { get; set; }
+
+        /// <summary>
+        /// Vector3 Z
+        /// </summary>
         [JsonProperty("z")]
         public float Z { get; set; }
     }
