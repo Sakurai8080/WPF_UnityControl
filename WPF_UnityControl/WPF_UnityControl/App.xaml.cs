912fe818efaa7ea03f3f51ada7edb2548196ad53
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-06-25 02:46:56 +0900

    App周りのリファクタリング

--- WPF_UnityControl/WPF_UnityControl/App.xaml.cs
+++ WPF_UnityControl/WPF_UnityControl/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using Prism.Unity;
-using Prism.Ioc;
-using System.Windows;
+﻿using System.Windows;
 using WPF_UnityControl.Views;
 using WPF_UnityControl.ViewModels;
 using WPF_UnityControl.Control;
@@ -10,7 +6,7 @@ using WPF_UnityControl.Facades;
 using WPF_UnityControl.Response;
 using WPF_UnityControl.Unity;
 using WPF_UnityControl.NetWork;
-using System.Net.Sockets;
+using WPF_UnityControl.Interface;
 
 namespace WPF_UnityControl;
 
@@ -25,19 +21,20 @@ public partial class App : PrismApplication
     }
 
     /// <summary>
-    /// コンテナへの型登録。ViewとViewModelの紐づけ
+    /// コンテナへのサービス登録(シングルトン・ViewとViewModelの紐づけ)
     /// </summary>
-    /// <param name="containerRegistry"></param>
+    /// <param name="containerRegistry">コンテナ管理</param>
     protected override void RegisterTypes(IContainerRegistry containerRegistry)
     {
-        containerRegistry.RegisterSingleton<SceneListResponse>();
-        containerRegistry.RegisterSingleton<HierarchyResponse>();
-        containerRegistry.RegisterSingleton<ObjectDataResponse>();
+        containerRegistry.RegisterSingleton<IResponseData,SceneListResponse>("SceneRes");
+        containerRegistry.RegisterSingleton<IResponseData, HierarchyResponse>("HieRarchyRes");
+        containerRegistry.RegisterSingleton<IResponseData, ObjectDataResponse>("ObjectRes");
+
         containerRegistry.RegisterSingleton<ResponseController>();
         containerRegistry.RegisterSingleton<TcpClientController>();
         containerRegistry.RegisterSingleton<UnityCommandDispatcher>();
-
         containerRegistry.RegisterSingleton<UnityController>();
+
         containerRegistry.RegisterForNavigation<MainContentPage>();
         containerRegistry.RegisterForNavigation<UnityOperationControl, UnityOperationControlViewModel>();
         containerRegistry.RegisterForNavigation<SceneListControl, SceneListControlViewModel>();
