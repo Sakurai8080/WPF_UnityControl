5e79b5adad14d888b5d57553e90affbe2d548eea
Sakurai8080 <sakuraitakuya8080@gmail.com>
2025-06-28 23:43:29 +0900

    接続状態のEventArgsを追加

---
+++ WPF_UnityControl/WPF_UnityControl/Unity/EventArgs/UnityConnectionEventArgs.cs
@@ -0,0 +1,32 @@
+﻿namespace WPF_UnityControl.Unity
+{
+    /// <summary>
+    /// Unity接続通知用データクラス
+    /// </summary>
+    public class UnityConnectionEventArgs : EventArgs
+    {
+        #region プロパティ
+        /// <summary>
+        /// 接続状態
+        /// </summary>
+        public bool IsConnected { get; }
+
+        /// <summary>
+        /// 接続メッセージ
+        /// </summary>
+        public string Message { get; }
+        #endregion
+        #region コンストラクタ
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="isConnected">接続状態</param>
+        /// <param name="message">接続メッセージ</param>
+        public UnityConnectionEventArgs(bool isConnected, string message)
+        {
+            IsConnected = isConnected;
+            Message = message;
+        }
+        #endregion
+    }
+}
